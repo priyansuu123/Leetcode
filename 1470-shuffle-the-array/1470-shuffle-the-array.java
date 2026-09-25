77d09fd5d82a6c00e7703ac641a73d722e107cac
priyansuu123 <priyanshurai821310@gmail.com>
2026-09-25 16:46:42 +0530

    Time: 0 ms (100%), Space: 46.4 MB (61.71%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int ans[]= new int[2*n];
+        int j =n;
+         for(int i=0;i<n;i++){
+            ans[2*i]=nums[i];
+            ans[2*i+1]= nums[j];
+            j++;
+         }
+         return ans;
+    }
+}
